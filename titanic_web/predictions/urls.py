a1c8a70957c6aebfa497aedeaf815db776b6333a
Arlann <handikaarlan@gmail.com>
2024-12-22 13:27:28 +0700

    refactor(predictions): simplify prediction input and improve ui

--- titanic_web/predictions/urls.py
+++ titanic_web/predictions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict/', views.predict, name='predict'),
+    path('', views.predict, name='predict'),  # Gunakan root path ('') tanpa prefix
 ]
